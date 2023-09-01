f438ea65b4d3718340c6d2c335ae37060f307e87
AdnaneTALEB <lm_taleb@esi.dz>
2023-09-01 18:37:52 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color){
+    let circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
